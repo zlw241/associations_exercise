cb92bc4eeb3e4193412e5aeeeedfd52d93b87d1c
App Academy Student <appacademy@Apps-Mac-mini-2.local>
2017-03-15 14:51:41 -0400

    Add table associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,32 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :integer          not null, primary key
+#  name       :string(255)
+#  created_at :datetime
+#  updated_at :datetime
+#
+
 class User < ActiveRecord::Base
+
+  has_many(
+    :enrollments,
+    :class_name => "Enrollment",
+    :foreign_key => :student_id,
+    :primary_key => :id
+  )
+
+  has_many(
+    :enrolled_courses,
+    :through => :enrollments,
+    :source => :course
+  )
+
+  has_many(
+    :instructed_courses,
+    :class_name => "Course",
+    :foreign_key => :instructor_id,
+    :primary_key => :id
+  )
 end
